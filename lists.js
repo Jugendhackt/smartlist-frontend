16213d2bb4b276b27e89e59d356b77fc0da5d6da
Moritz Schulz <moritzleni@gmail.com>
2018-08-11 19:52:00 +0200

    now able to add to other lists

--- lists.js
+++ lists.js
@@ -1,3 +1,5 @@
+var listID = -1;
+
 function removeElement(elment) {
 elment.parentNode.removeChild(elment);
 }
@@ -71,7 +73,7 @@ function addlistItem(Text, send, isEntry) {
 	    var t = prompt('Please enter the Item', list.textContent);
     	    if(t) {
     	      list.textContent = t;
-      	    sendRequest("http://192.168.21.160:3000/lists/0/entries/"+list.id+"/edit", list.textContent, null, "PUT");
+      	    sendRequest("http://192.168.21.160:3000/lists/"+listID+"/entries/"+list.id+"/edit", list.textContent, null, "PUT");
           }else if(t !== null){
             console.log("Bool");
             removeElement(list);
@@ -81,10 +83,11 @@ function addlistItem(Text, send, isEntry) {
 	    list.addEventListener('click', function(e) {
 	    emptyList()
        	    loadEntries("http://192.168.21.160:3000/lists/"+list.id);
+        listID = list.id;
 	});
 	}
 	
-    if(send)sendRequest("http://192.168.21.160:3000/lists/0/entries/add", Text, list, "POST");
+    if(send)sendRequest("http://192.168.21.160:3000/lists/"+listID+"/entries/add", Text, list, "POST");
 	return list;
 }
 
