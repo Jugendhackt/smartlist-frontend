43d288d030934b8f5552e8fc14c7305761974b6c
Blockcrafter Hd <schneider.franz.1234@gmail.com>
2018-08-11 14:53:41 +0200

    Edit Promt Added

--- lists.js
+++ lists.js
@@ -4,7 +4,13 @@ function addlistItem(Text, edit) {
 	 var list = document.createElement("LI");
 	 list.contentEditable  = edit;
     var textelement = document.createTextNode(Text);
+    list.id = "li";
     list.appendChild(textelement);
     ul.appendChild(list);
 }
-addlistItem("Test Text", true);
+
+ document.addEventListener('click', function(e) {
+    e = e || window.event;
+    target = e.target || e.srcElement; 
+    if(target.id == "li") target.textContent = prompt('Please enter the Item');
+ });
