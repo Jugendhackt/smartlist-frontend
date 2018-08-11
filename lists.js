eff38b845c5ffaced09eba0781ec9fba30257e48
Blockcrafter Hd <schneider.franz.1234@gmail.com>
2018-08-11 14:32:38 +0200

    added edit options

--- lists.js
+++ lists.js
@@ -1,8 +1,10 @@
 var ul = document.getElementById("overview");
-function addlistItem(Text) {
+function addlistItem(Text, edit) {
+	if(edit != true && edit != false) edit = false;
 	 var list = document.createElement("LI");
+	 list.contentEditable  = edit;
     var textelement = document.createTextNode(Text);
     list.appendChild(textelement);
     ul.appendChild(list);
 }
-addlistItem("Test Text");
+addlistItem("Test Text", true);
