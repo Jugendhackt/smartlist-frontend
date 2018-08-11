d36663d3fb4a466f600052c28c4d4f3a212107a3
Blockcrafter Hd <schneider.franz.1234@gmail.com>
2018-08-11 18:05:58 +0200

    Bug Fixes-Cancel doesn´t create Empty elment

--- lists.js
+++ lists.js
@@ -10,8 +10,10 @@ function makeid() {
   return text;
 }
 
-
-
+function add() {
+	var t = prompt('Please enter the Item');
+	if(t) addlistItem(t);
+}
 
 function sendRequest(website, Text, element, methode) {
 let request = new XMLHttpRequest();
@@ -19,9 +21,11 @@ let request = new XMLHttpRequest();
     if (this.readyState == 4 && this.status == 200) {
     	json = this.responseText;
     	json = JSON.parse(json);
-    	console.log(json);
-    	console.log(json["id"]);
-      element.id = json["id"];
+    	if(element !== null) {
+      element.id = json.id;
+      element.textContent = json.entry.text;
+
+      }
     }
   };
   request.open(methode, website, true);
@@ -29,7 +33,10 @@ let request = new XMLHttpRequest();
   request.send(JSON.stringify({entry:{text:Text, category:"Der Himmel ist Blau .ung if bim schlau"}}));
 }
 
+function load() {
 
+}
+load();
 function addlistItem(Text) {
 	var ul = document.getElementById("overview");
 	 var list = document.createElement("LI");
@@ -38,8 +45,11 @@ function addlistItem(Text) {
     list.appendChild(textelement);
     ul.appendChild(list);
     list.addEventListener('click', function(e) {
-    	list.textContent = prompt('Please enter the Item', list.textContent);
-    	sendRequest("http://192.168.21.160:3000/lists/0/entries/"+list.id+"/edit", list.textContent, list, "PUT");
+    	var t = prompt('Please enter the Item', list.textContent);
+    	if(t) {
+    	list.textContent = t;
+    	sendRequest("http://192.168.21.160:3000/lists/0/entries/"+list.id+"/edit", list.textContent, null, "PUT");
+        }
     });
     sendRequest("http://192.168.21.160:3000/lists/0/entries/add", Text, list, "POST");
 }
