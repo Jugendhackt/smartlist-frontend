cad854296bc9f65489c3a9a0f1e3f9fb94f6eaf4
Blockcrafter Hd <schneider.franz.1234@gmail.com>
2018-08-11 16:56:43 +0200

    Post/Put Request function added

--- lists.js
+++ lists.js
@@ -1,5 +1,4 @@
-var ul = document.getElementById("overview");
-
+var json = " ";
 //copy from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function makeid() {
   var text = "";
@@ -11,16 +10,28 @@ function makeid() {
   return text;
 }
 
-function addrequest(Text) {
-return makeid();
+
+
+
+function sendRequest(website, Text, element, methode) {
+let request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    	json = this.responseText;
+    	json = JSON.parse(json);
+    	console.log(json);
+    	console.log(json["id"]);
+      element.id = json["id"];
+    }
+  };
+  request.open(methode, website, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(JSON.stringify({text:Text, category:"Der Himmel ist Blau .ung if bim schlau"}));
 }
 
-function changerequest(Text) {
-	
-}
 
-function addlistItem(Text, edit) {
-	if(edit != true && edit != false) edit = false;
+function addlistItem(Text) {
+	var ul = document.getElementById("overview");
 	 var list = document.createElement("LI");
     var textelement = document.createTextNode(Text);
 
@@ -28,10 +39,8 @@ function addlistItem(Text, edit) {
     ul.appendChild(list);
     list.addEventListener('click', function(e) {
     	list.textContent = prompt('Please enter the Item', list.textContent);
- });
-    if(!edit) {
-    list.id = addrequest(Text);
-    }else{
-    	changerequest();
-    } 
+    	sendRequest("http://192.168.21.160:3000/lists/0/entries/"+list.id+"/edit", Text, "PUT");
+    });
+    sendRequest("http://192.168.21.160:3000/lists/0/entries/add", Text, list, "POST");
 }
+
